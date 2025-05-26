5808b40b92e1342197a5902afbe890cf175ec0e5
olopok <pabisa.pb@gmail.com>
2025-05-26 19:10:04 +0100

    Add methods set and get

--- hashmap.js
+++ hashmap.js
@@ -1,1 +1,47 @@
+import LinkedList from "./linkedlist.js";
 
+export class HashMap {
+  capacity = 16;
+  loadFactor = 0.75;
+  growingAt = Math.ceil(this.capacity * this.loadFactor);
+  buckets = new Array(this.capacity).fill(null).map(() => []);
+
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+    hashCode = hashCode % this.capacity;
+    return hashCode;
+  }
+
+  set(key, value) {
+    const index = this.hash(key);
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error("Trying to access index out of bounds");
+    }
+    const bucket = [key, value];
+    if (!this.buckets[index]) {
+      this.buckets[index] = [bucket];
+      return;
+    }
+    for (let i = 0; i < this.buckets[index].length; i++) {
+      if (this.buckets[index][i][0] === key) {
+        this.buckets[index][i][1] = value;
+        return;
+      }
+    }
+    this.buckets[index].push(bucket);
+  }
+
+  get(key) {
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        return console.log(`The value of ${key} is: ${bucket[i][1]}`);
+      } else return console.log(`The key ${key} not found:`, null);
+    }
+  }
+}
--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+import { HashMap } from "./hashmap.js";
 
+const test = new HashMap();
+
+test.set("apple", "red");
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+test.set("dog", "brown");
+test.set("elephant", "gray");
+test.set("frog", "green");
+test.set("grape", "purple");
+test.set("hat", "black");
+test.set("ice cream", "white");
+test.set("jacket", "blue");
+test.set("kite", "pink");
+test.set("lion", "golden");
+
+test.set("Banana", "brown");
+
+test.get("lon");
+
+console.log("test", test.buckets);

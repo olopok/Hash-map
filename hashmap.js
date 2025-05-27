0344dee83e23844c8070f9f8765ac09c253bf52a
olopok <pabisa.pb@gmail.com>
2025-05-27 19:42:11 +0100

    Add has and remove methods.

--- hashmap.js
+++ hashmap.js
@@ -1,5 +1,3 @@
-import LinkedList from "./linkedlist.js";
-
 export class HashMap {
   capacity = 16;
   loadFactor = 0.75;
@@ -18,9 +16,15 @@ export class HashMap {
 
   set(key, value) {
     const index = this.hash(key);
+
+    if (this.length() >= this.growingAt) {
+      this.resize();
+    }
+
     if (index < 0 || index >= this.buckets.length) {
       throw new Error("Trying to access index out of bounds");
     }
+
     const bucket = [key, value];
     if (!this.buckets[index]) {
       this.buckets[index] = [bucket];
@@ -43,5 +47,95 @@ export class HashMap {
         return console.log(`The value of ${key} is: ${bucket[i][1]}`);
       } else return console.log(`The key ${key} not found:`, null);
     }
+  }
+
+  has(key) {
+    for (let i = 0; i < this.buckets.length; i++) {
+      for (let elem of this.buckets[i]) {
+        if (elem[0] === key) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  remove(key) {
+    if (this.has(key)) {
+      const index = this.hash(key);
+      const bucket = this.buckets[index];
+      for (let arr of this.buckets[index]) {
+        if (arr[0] === key) {
+          const entryIndex = this.buckets[index].indexOf(arr);
+          this.buckets[index].splice(entryIndex, 1);
+          console.log("remove", this.buckets);
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  length() {
+    let entries = 0;
+    for (let i = 0; i < this.buckets.length; i++) {
+      if (this.buckets[i]) {
+        for (let arr of this.buckets[i]) {
+          if (arr) {
+            entries++;
+          }
+        }
+      }
+    }
+    return entries;
+  }
+
+  clear() {
+    this.buckets = new Array(this.capacity).fill(null).map(() => []);
+    return this.buckets;
+  }
+
+  keys() {
+    const keys = [];
+    for (let i in this.buckets) {
+      for (let element of this.buckets[i]) {
+        keys.push(element[0]);
+      }
+    }
+    return keys;
+  }
+
+  values() {
+    const values = [];
+    for (let i in this.buckets) {
+      for (let element of this.buckets[i]) {
+        values.push(element[1]);
+      }
+    }
+    return values;
+  }
+
+  entries() {
+    const entriesArr = [];
+    for (let i in this.buckets) {
+      for (let element of this.buckets[i]) {
+        if (element) {
+          entriesArr.push(element);
+        }
+      }
+    }
+    return entriesArr;
+  }
+
+  resize() {
+    this.capacity *= 2;
+    let buckets = new Array(this.capacity).fill(null).map(() => []);
+    const entries = this.entries();
+    for (let arr of entries) {
+      let index = this.hash(arr[0]);
+      buckets[index].push(arr);
+    }
+    this.buckets = buckets;
+    this.growingAt = Math.ceil(this.capacity * this.loadFactor);
   }
 }
--- main.js
+++ main.js
@@ -16,7 +16,14 @@ test.set("kite", "pink");
 test.set("lion", "golden");
 
 test.set("Banana", "brown");
+test.set("bella", "figa");
 
-test.get("lon");
-
-console.log("test", test.buckets);
+console.log(test.get("lon")); //return false, lon don't exsit.
+console.log(test.has("Banana"));//return true
+console.log(test.remove("kite"));// kite was removed
+console.log(test.buckets);// reurn the full hash map
+console.log(test.length());// return the number of the entries
+console.log(test.keys())// return all the keys
+console.log(test.values())// return all the values
+console.log(test.entries())// return all the entries
+console.log(test.clear());// return an empty hash map

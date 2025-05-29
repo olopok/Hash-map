1c14302abd348742d9fe2ee6381fabd0dde8f356
olopok <pabisa.pb@gmail.com>
2025-05-29 19:45:08 +0100

    All methods are done for HashMap class

--- hashmap.js
+++ hashmap.js
@@ -75,7 +75,6 @@ export class HashMap {
     }
     return false;
   }
-<<<<<<< HEAD
 
   length() {
     let entries = 0;
@@ -139,6 +138,4 @@ export class HashMap {
     this.buckets = buckets;
     this.growingAt = Math.ceil(this.capacity * this.loadFactor);
   }
-=======
->>>>>>> 8aa9539b0f5a3a8aa3d7923259d7d572d14cbbac
 }
